ca8375b2569b955782032693500f3048e35fc1e8
abrab <abdelmoughit.rabia2@gmail.com>
2019-11-06 15:28:37 +0000

    pass unkown props

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 // import Radium from 'radium';
 import Aux from '../../../hoc/Auxilary';
+import withClass from '../../../hoc/withClass';
 
 import classes from './Person.css';
 
@@ -10,15 +11,13 @@ class Person extends Component {
     console.log('[Person.js] rendering...');
         return (
             <Aux>
-            {/* <div className={classes.Person}> */}
                 <p onClick={this.props.click}>I'm {this.props.name} and i will get rich after this.{this.props.years} years !!</p>
                 <p>{this.props.children}</p>
                 <input  type='text' onChange={this.props.changed} value={this.props.name}></input>
-             {/* </div> */}
             </Aux>
         )
     }
 }
 
 // export default Radium(person);
-export default Person;
+export default withClass(Person, classes.Person);
--- src/hoc/WithClass.js
+++ src/hoc/WithClass.js
@@ -3,7 +3,7 @@ import React from 'react';
 const withClass = (WrapedComponent,className) => {
     return props => (
         <div className={className}>
-            <WrapedComponent />
+            <WrapedComponent {...props}/>
         </div>
     );
 };
