bdd5580bd6d254fe3a5914896d91e582af4c9e5e
abrab <abdelmoughit.rabia2@gmail.com>
2019-11-06 14:52:09 +0000

    functional HOCs

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
-import WithClass from '../hoc/WithClass';
+import withClass from '../hoc/withClass';
+import Aux from '../hoc/Auxilary';
 
 
 class App extends Component {
@@ -77,7 +78,7 @@ class App extends Component {
     }
 
     return (
-        <WithClass classes={classes.App}>
+        <Aux>
             <button
                 onClick={() =>{this.setState({ showCockpit: !this.state.showCockpit })
             }}>
@@ -91,9 +92,9 @@ class App extends Component {
                     showPersons={this.state.showPersons} />
             : null}
                 {persons}
-        </WithClass>
+        </Aux>
     );
 
   }
 }
-export default App;
+export default withClass(App, classes.App);
--- src/hoc/WithClass.js
+++ src/hoc/WithClass.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const withClass = (props) => (
-    <div className={props.classes}>
-        {props.children}
-    </div>
-);
+const withClass = (WrapedComponent,className) => {
+    return props => (
+        <div className={className}>
+            <WrapedComponent />
+        </div>
+    );
+};
 
 export default withClass;
