8244324476d30476e4d8ab147db831aed2b2953b
abrab93 <abrab93@gmail.com>
2019-11-08 14:55:54 +0000

    ctrl shift F

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 // import Radium from 'radium';
 import Aux from '../../../hoc/Auxilary';
 import withClass from '../../../hoc/withClass';
@@ -8,24 +8,24 @@ import AuthContext from '../../../context/auth-context';
 import classes from './Person.css';
 
 class Person extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.inputElemRef = React.createRef();
     }
 
-// must be static *contextType* so we can use this.context where ever we want in the component
+    // must be static *contextType* so we can use this.context where ever we want in the component
     static contextType = AuthContext;
 
-    componentDidMount(){
+    componentDidMount() {
         this.inputElemRef.current.focus();
         console.log(this.context.authenticated);
     }
 
-    render(){
-    console.log('[Person.js] rendering...');
+    render() {
+        console.log('[Person.js] rendering...');
         return (
             <Aux>
-                { this.context.authenticated ? <p> Authenticated </p> : <p> Please Log in !!</p>}
+                {this.context.authenticated ? <p> Authenticated </p> : <p> Please Log in !!</p>}
                 <p onClick={this.props.click}>I'm {this.props.name} and i will get rich after this.{this.props.years} years !!</p>
                 <p>{this.props.children}</p>
                 <input
